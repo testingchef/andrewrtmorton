d21801e3d76daed72aa99d7be55e4ce515f564c4
Andrew Morton <andrew@ministryoftesting.com>
2025-10-18 11:08:16 +0100

    Add homepage

---
+++ app/controllers/defensive_controller.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class DefensiveController < ApplicationController
+end
---
+++ app/controllers/homepage_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class HomepageController < DefensiveController
+
+  def index
+
+  end
+
+  def show
+    @homepage = Homepage.new
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
